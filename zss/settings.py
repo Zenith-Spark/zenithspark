aaffbd1becfd8ab5358556c0492a140499a5fcaa
Zenith-Spark <zenithsparkstation@gmail.com>
2024-11-08 14:29:41 +0100

    forgot password fixed

--- zss/settings.py
+++ zss/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -191,7 +190,6 @@ AUTHENTICATION_BACKENDS = [
 
 MAX_UPLOAD_SIZE = 5242880 
 
-load_dotenv()
 
 
 # Email Configuration
@@ -199,7 +197,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'zenithsparkstation@gmail.com'
+EMAIL_HOST_USER = 'zenithsparkstation@gmail.com'
+EMAIL_HOST_PASSWORD = 'ngyqmqzicojalncv '
+DEFAULT_FROM_EMAIL = 'Zenith Spark Station <zenithsparkstation@gmail.com>'
 
